8a2be45af0079584c0c6ceb1f6e8e2f805d77ca2
Zuozhi Wang <zuozhiw@gmail.com>
2022-09-01 07:20:41 -0700

    Add Crawler blog post (#6)
    This PR adds an initial draft of the crawler blog post

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,6 +1,7 @@
 import hljs from 'highlight.js/lib/core';
 
 import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
 import json from 'highlight.js/lib/languages/json';
 import bash from 'highlight.js/lib/languages/bash';
 import xml from 'highlight.js/lib/languages/xml';
@@ -8,6 +9,7 @@ import ini from 'highlight.js/lib/languages/ini';
 import yaml from 'highlight.js/lib/languages/yaml';
 import markdown from 'highlight.js/lib/languages/markdown';
 
+hljs.registerLanguage('python', python);
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('bash', bash);
